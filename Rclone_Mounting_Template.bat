8e7a8017ad6a6eb1da71ecc91f30521bb4f53113
C̸̯̭̈́̓̿̄̆͠ͅo̴̭̣̩͌̓̎̉͐͂̈́̀͝d̸̡̗̀̇͝ȩ̷͕̲͔̗̤̰͆̏-̵̧̫̰̰̠̾̽͆̌͜M̸̬̹͚̠̖͖̻̾o̵̡̢̘̯͚̯̤͎̺͊̑̈́̂̒̐̎̓s̸̯̬̀͌̔̊͒̓̈́̕͝͝s̶̱̺̏̽̋̑̿ <74435632+Code-Moss@users.noreply.github.com>
2021-10-19 21:25:07 +0000

    Update Rclone_Mounting_Template.bat

--- Rclone_Mounting_Template.bat
+++ Rclone_Mounting_Template.bat
@@ -1,26 +1,18 @@
 
 ::rclone Mounting Template: https://github.com/Code-Moss/Rclone-Mounting-Template
-
+::
 ::WARNING: Do NOT run using Administrator privileges.
-
 ::Once the service has started, check your designated drive.
 ::When you are done using your Storage System, you may safely close the command prompt.
-
+::
 ::Pre-Requisites:
-
+::
 ::Rclone: https://rclone.org/downloads/
 ::An already created Rclone Remote: eg. Google Drive: https://rclone.org/drive/ (check the website to see other supported storage systems). 
 ::WinFSP: https://github.com/billziss-gh/winfsp/releases/ (The Latest Release is reccomended).
 
 
-
-
-
-::Script Start-----
-
 @echo off
 cd C:\rclone\
 rclone mount --vfs-cache-mode full "Your Remote Here": X:
 pause
-
-::Script End-----
